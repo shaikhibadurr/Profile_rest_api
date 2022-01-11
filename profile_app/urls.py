75759f75cff642083b257eff69db94c117ea88be
Ibadurrahman <shaikhibadurr@gmail.com>
2022-01-11 09:04:51 +0530

    Added all the thing

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -4,7 +4,10 @@ from profile_app import views
 
 router=DefaultRouter()
 router.register('hello-viewsets',views.HelloViewSets, basename='hello-viewset')
+router.register('profile',views.UserProfileViewset)
+router.register('feed',views.UserProfileFeedViewSet)
 urlpatterns=[
     path('hello-view/',views.HelloApiView.as_view()),
+    path('login/',views.UserLoginApiView.as_view()),
     path('',include(router.urls)),
 ]
--- profile_app/views.py
+++ profile_app/views.py
@@ -3,7 +3,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from profile_app import serializers
 from rest_framework import viewsets
-
+from profile_app import models
+from rest_framework.authentication import TokenAuthentication
+from profile_app import permissions
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 class HelloApiView(APIView):
     serializer_class=serializers.HelloSerializer
     def get(self,request,format=None):
@@ -52,3 +58,25 @@ class HelloViewSets(viewsets.ViewSet):
         return Response({'method':'Partial update'})
     def destroy(self,request,pk=None):
         return Response({'method':'destroy'})
+
+class UserProfileViewset(viewsets.ModelViewSet):
+    serializer_class=serializers.UserProfileSerializer
+    queryset=models.UserProfile.objects.all()
+    authentication_classes=(TokenAuthentication,)
+    permission_classes=(permissions.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name','email',)
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes=api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    authentication_classes=(TokenAuthentication,)
+    serializer_class=serializers.ProfileFeedItemSerializer
+    queryset=models.ProfileFeedItem.objects.all()
+    permission_classes=(
+        permissions.UpdateOwnStatus,
+        IsAuthenticated
+    )
+    def perform_create(self,serializer):
+        serializer.save(user_profile=self.request.user)
